00a4844645e4de379da35f19616a98d6286718b8
Utsav Baral <manutsssav@gmail.com>
2024-09-02 07:56:26 +0545

    remove sidebar on mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
+// import { Button } from "./ui/button";
 import Nav from "./Nav";
-import MobileNav from "./MobileNav";
+// import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -18,15 +18,15 @@ const Header = () => {
 
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
-          <Link href={"/contact"}>
+          {/* <Link href={"/contact"}>
             <Button>Hire me </Button>
-          </Link>
+          </Link> */}
         </div>
 
         {/* mobile nav*/}
-        <div className="xl:hidden">
+        {/* <div className="xl:hidden">
           <MobileNav />
-        </div>
+        </div> */}
       </div>
     </header>
   );
