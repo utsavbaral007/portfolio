a35936bc24ecd4eafe42ca47b9f375c79ceec017
Utsav Baral <manutsssav@gmail.com>
2024-09-23 10:25:38 +0545

    next config changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
-  serverRuntimeConfig: {
+  ServerRuntimeConfig: {
     PROJECT_ROOT: __dirname,
   },
   webpack: (config) => {
