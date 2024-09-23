69a067a9dbf1d175c91451db56705f7a866dd41b
Diwas KC <37412746+d1was@users.noreply.github.com>
2024-09-23 09:59:07 +0545

    Update nodemailer.js

--- src/lib/nodemailer.js
+++ src/lib/nodemailer.js
@@ -18,7 +18,7 @@ const handlebarOptions = {
   viewEngine: {
     dfaultLayout: false,
   },
-  viewPath: "../app/template",
+  viewPath: "app/template",
 };
 
 transporter.use("compile", hbs(handlebarOptions));
